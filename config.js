62f1761e28fc943467557aca9eae3eaf7bcd9fad
2tad <94532313+2tad@users.noreply.github.com>
2021-11-17 18:50:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
     titleWeb: "I LOVE U 3000",
-    introTitle: 'TỚ THÍCH CẬU NHÌU LẮM',
+    introTitle: 'NÈ CẬU ƠI',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao cậu cứ đi nhầm đường
-    Lạc vào tim tớ lẻ loi
+    mà sao cậu cứ đi nhầm đường
+    lạc vào tim tớ lẻ loi.
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng Diên đã yêu ngay từ cái nhìn đầu tiên :>',
-    desc: 'Phải chăng Diên đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu Diên <3',
-    btnReply: 'Gửi cho cậu <3',
+    btnIntro: '^^ Hénooo^^',
+    title: 'Cậu độc thân, tớ cũng độc thân. Chắc là không phải ngẫu nhiên đâu nhỉ🥺 ',
+    desc: 'Hay là đôi ta đến zới nhau đee 🙄',
+    btnYes: 'Ỏooo đồng ý 😻',
+    btnNo: 'Ờ hớ hông nhoa 😼',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao cậu lại chọn yêu Diên zợ <3',
+    btnReply: 'Gửi Diên nè <3',
     reply: 'Yêu thì yêu mà không yêu thì cũng phải yêu Diên nhé <33333333',
-    mess: 'Mình biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
-    messDesc: 'khi nào mình đi chơi nha yêu cậu nhiều',
-    btnAccept: 'Uciiii lun nà <3',
+    mess: 'UwU mình biết mà, ngang ngược we 😖. Yêu cậu nhìu quá lun á chời 💗',
+    messDesc: ' 🤭 Thế từ nay cậu là bồ tớ nha yêu cậu nhìu nhìu ',
+    btnAccept: 'Uciiii lun nà ♡',
     messLink: 'https://https://www.facebook.com/profile.php?id=100061241004002' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
